6757c7088b2ddd940f7cbf78ba23e94829ceead3
Malhar Ujawane <Malharujawane@gmail.com>
2024-10-26 08:16:25 +0530

    Update branding.ts

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'M-Y AI Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = 'M-Y AI';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'M-Y Hub';
 
 export const BRANDING_URL = {
   help: undefined,
